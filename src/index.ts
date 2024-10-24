1cc3ca5f6e642cd6e554fc929dee08cc5ea89495
keithwalsh <keithwalsh@gmail.com>
2024-10-24 23:59:17 +0100

    fix: correct export type names

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+/**
+ * @fileoverview Entry point for the MenuBar component. Exports the main component
+ * and its associated types for external use.
+ */
+
 // Export the MenuStrip component
 export { default as MenuBar } from "./components/MenuBar";
 
 // Export types using 'export type'
-export type { MenuConfig, MenuBarItem, MenuBarAction, MenuBarDivider } from "./components/MenuBar";
+export type { MenuConfig, MenuItems, MenuItemAction, MenuItemDivider, MenuItemSubmenu } from "./components/MenuBar";
